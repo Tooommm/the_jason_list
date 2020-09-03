db3165c48ae6f24c336d3c6c3604b3b8cf79bf37
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-09-04 00:19:20 +0200

    persister en BDD

--- app/controllers/argonautes_controller.rb
+++ app/controllers/argonautes_controller.rb
@@ -1,2 +1,19 @@
 class ArgonautesController < ApplicationController
+
+  def create
+    @argonaute = Argonaute.new(argonaute_params)
+    if @argonaute.save
+      redirect_to root_path
+    else
+      raise
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def argonaute_params
+    params.require(:argonaute).permit(:name, :half_god, :notes)
+  end
+
 end
